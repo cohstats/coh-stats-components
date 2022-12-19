d309b972ee1c94778c5100a97708d0baaaccea00
Johannes Merkt <mailjohannesmerkt@gmail.com>
2022-12-19 23:46:47 +0100

    Add ColorSchemeToggle

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,5 @@
 export { TestComponent } from './TestComponent/TestComponent';
 export type { TestComponentProps } from './TestComponent/TestComponent';
+export { IconButton } from "./IconButton/IconButton";
+export type { IconButtonProps } from "./IconButton/IconButton";
+export { ColorSchemeToggle } from "./ColorSchemeToggle/ColorSchemeToggle";
